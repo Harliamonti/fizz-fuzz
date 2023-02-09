5e61c161e8fc547fea69d7dce2e4a20831e7c0bf
Harliamonti <123170910+Harliamonti@users.noreply.github.com>
2023-02-09 11:10:15 +0200

    Very basic fizzbuzz code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
 
+void fizzBuzzSimple();
+
 int main () 
 {
-    std::cout << "Hello World!" << std::endl;
+    fizzBuzzSimple();
+}
+
+void fizzBuzzSimple()
+{
+    bool isFizzBuzz{false};
+
+    for (size_t i{1}; i <= 100; i++)
+    {
+        if (i % 3 == 0)
+        {
+           std::cout << "fizz";
+           isFizzBuzz = true;
+        }
+        if (i % 5 == 0)
+        {
+            std::cout << "buzz";
+            isFizzBuzz = true;
+        }   
+        if (isFizzBuzz == false)
+        {
+            std::cout << i << std::endl;
+        }
+        else
+            std::cout << std::endl;
+            isFizzBuzz = false;
+    } 
 }
